0dacaefbb11f97f867088be8a6abc8105f53ee9e
jeonghyeon <jhjhjh96@huve.co.kr>
2020-05-06 14:54:40 +0900

    Implementing Data Structure Stack with Go

--- data/stack/arrayStack.go
+++ data/stack/arrayStack.go
@@ -1,5 +1,7 @@
 package stack
 
+import "errors"
+
 type ArrayStack struct {
 	*Stack
 	itemArray []arrayElement
@@ -20,8 +22,8 @@ func NewArrayStack(capacity int) *ArrayStack {
 // Initialize the ArrayStack.
 func (stack *ArrayStack) Init(capacity int) *ArrayStack {
 	stack.Stack = initializeParent()
+	stack.itemArray = make([]arrayElement, capacity)
 	stack.maxSize = capacity
-	stack.itemArray = nil
 	stack.pointer = 0
 	return stack
 }
@@ -34,4 +36,40 @@ func (stack *ArrayStack) Capacity() int {
 // Check if the Stack is full.
 func (stack *ArrayStack) IsFull() bool {
 	return stack.Capacity() == stack.size
+}
+
+// Push a new item on the top.
+func (stack *ArrayStack) Push(value interface{}) error {
+	newElement := arrayElement{
+		index: stack.size,
+		value: value,
+	}
+	endIndex := newElement.index
+	if endIndex >= stack.Capacity() {
+		return errors.New("capacity exceeded")
+	} else {
+		stack.itemArray[endIndex] = newElement
+		stack.pointer = endIndex
+		stack.size++
+		return nil
+	}
+}
+
+// Top item returns. The objects returned will be removed.
+func (stack *ArrayStack) Pop() interface{} {
+	if stack.IsEmpty() {
+		return nil
+	} else {
+		outObject := stack.itemArray[stack.pointer]
+		stack.itemArray[stack.pointer] = arrayElement{}
+		stack.pointer--
+		stack.size--
+		return outObject.value
+	}
+}
+
+// Top item returns. Unlike 'Pop()', the top item returned will be maintained.
+func (stack *ArrayStack) Peek() interface{} {
+	outObject := stack.itemArray[stack.pointer]
+	return outObject.value
 }
--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 
 func main() {
 	//exampleLinkedStack()
+	//exampleArrayStack()
 }
+
+//region Stack example
 
 // Example of using LinkedStack
 func exampleLinkedStack() {
@@ -36,3 +39,57 @@ func exampleLinkedStack() {
 		log.Println("IsEmpty:", newInstance.IsEmpty())
 	}
 }
+
+// Example of using ArrayStack
+func exampleArrayStack() {
+	newInstance := stack.NewArrayStack(4)
+
+	err := newInstance.Push("Out")
+	if err == nil {
+		log.Printf("Added new one, Current size is %d\n", newInstance.Size())
+	} else {
+		log.Println(err)
+	}
+
+	err = newInstance.Push("Last")
+	if err == nil {
+		log.Printf("Added new one, Current size is %d\n", newInstance.Size())
+	} else {
+		log.Println(err)
+	}
+
+	err = newInstance.Push("Input")
+	if err == nil {
+		log.Printf("Added new one, Current size is %d\n", newInstance.Size())
+	} else {
+		log.Println(err)
+	}
+
+	err = newInstance.Push("First")
+	if err == nil {
+		log.Printf("Added new one, Current size is %d\n", newInstance.Size())
+	} else {
+		log.Println(err)
+	}
+
+	err = newInstance.Push("Firs232323t")
+	if err == nil {
+		log.Printf("Added new one, Current size is %d\n", newInstance.Size())
+	} else {
+		log.Println(err)
+	}
+
+	itemSize := newInstance.Size()
+	for i := 0; i < itemSize; i++ {
+		outValue := newInstance.Peek()
+		log.Println("Peek value:", outValue, " Current size is : ", newInstance.Size())
+	}
+
+	for i := 0; i < itemSize; i++ {
+		outValue := newInstance.Pop()
+		log.Println("Pop value:", outValue, " Current size is : ", newInstance.Size())
+		log.Println("IsEmpty:", newInstance.IsEmpty())
+	}
+}
+
+//endregion Stack example
